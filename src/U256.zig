f3665682e5d74e58cecac17d05a18c448c272605
Mizuochi Keita <keitam913@yahoo.co.jp>
2023-04-15 12:24:51 +0900

    Define add()

--- src/U256.zig
+++ src/U256.zig
@@ -26,6 +26,19 @@ pub fn format(self: *const Self, comptime _: []const u8, _: std.fmt.FormatOption
     try writer.print("{x:0>16}{x:0>16}{x:0>16}{x:0>16}", .{ self.data[3], self.data[2], self.data[1], self.data[0] });
 }
 
+pub fn add(x: *Self, y: *Self) Self {
+    var z = Self{ .data = [4]u64{ 0, 0, 0, 0 } };
+    var carry: u64 = 0;
+    var i: u8 = 0;
+    while (i < 4) : (i += 1) {
+        const v = @addWithOverflow(x.data[i], y.data[i]);
+        const w = @addWithOverflow(v[0], carry);
+        z.data[i] = w[0];
+        carry = v[1] + w[1];
+    }
+    return z;
+}
+
 test "fromHex" {
     {
         const actual = try fromHex("ff");
@@ -47,5 +60,18 @@ test "format" {
         const actual = try fmt.allocPrint(testing.allocator, "{}", .{try fromHex("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")});
         defer testing.allocator.free(actual);
         try testing.expectEqualStrings("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", actual);
+    }
+}
+
+test "add" {
+    {
+        var x = try fromHex("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        var y = try fromHex("5555555555555555555555555555555555555555555555555555555555555555");
+        try testing.expectEqualDeep(try fromHex("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"), x.add(&y));
+    }
+    {
+        var x = try fromHex("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+        var y = try fromHex("1");
+        try testing.expectEqualDeep(try fromHex("0"), x.add(&y));
     }
 }
